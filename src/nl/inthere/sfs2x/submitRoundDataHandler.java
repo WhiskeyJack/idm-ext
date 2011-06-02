495bf02c1b9956bb668ebb596c8f018559d93187
Bert Santema <b.santema@inthere.nl>
2011-06-02 22:37:00 +0200

    Added cleanup of serverside playerdata list
    Change-Id: If960f103b04dd4fc890ba2d2f8baa0842e7b6fce

--- src/nl/inthere/sfs2x/submitRoundDataHandler.java
+++ src/nl/inthere/sfs2x/submitRoundDataHandler.java
@@ -13,10 +13,12 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 	
 	@Override
 	public void handleClientRequest(User player, ISFSObject params) {
-		trace("Submit rounddata handler called");
+		// trace("Submit rounddata handler called");
 		
 		IdmExt parentEx = (IdmExt) getParentExtension();	
 		SFSObject playerRoundData = (SFSObject)params.getSFSObject("roundData");
+		
+		trace ("Size of playerDataList at start: " + parentEx.playerDataList.size() );
 		
 		int playerId = player.getPlayerId();
 		Room playerRoom = player.getLastJoinedRoom(); 
@@ -24,7 +26,7 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 		int currentRound = playerRoundData.getInt("round");
 		int playerCount = playerRoom.getSize().getUserCount();
 		
-		trace ("Received data for round " + currentRound + " and player " + playerId);
+		// trace ("Received data for round " + currentRound + " and player " + playerId);
 		
 		 // add round data to playerdata list
 		 if ( parentEx.playerDataList.size() == 0)
@@ -39,9 +41,9 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 			 for (PlayerData pd : parentEx.playerDataList )
 			 {
 				// check for duplicate, if do not add
-				trace("Checking for duplicate");
-				trace("if (pd.room == roomId && pd.playerId == playerId && pd.round == currentRound)");
-				trace("if " + pd.room + " == " + roomId + " && " + pd.playerId + " == " +playerId + "&& " + pd.round + " == " + currentRound);
+				// trace("Checking for duplicate");
+				// trace("if (pd.room == roomId && pd.playerId == playerId && pd.round == currentRound)");
+				// trace("if " + pd.room + " == " + roomId + " && " + pd.playerId + " == " +playerId + "&& " + pd.round + " == " + currentRound);
 				if (pd.room == roomId && pd.playerId == playerId && pd.round == currentRound)
 				{
 					found = true;
@@ -53,32 +55,32 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 			 }
 			 if (!found)
 			 {
-			 trace ("adding player round datat to parent list");
+			 // trace ("adding player round datat to parent list");
 				AddPlayerDataToList(player, playerRoundData, parentEx);
 			 }
 		}
 		 
 		 // check if we have data for all players for this round by putting counting the list
 		 List<PlayerData> playerDataRound = new ArrayList<PlayerData>();
-		 trace("starting parentEx.playerDataList loop to count if we have all players' data");
+		 // trace("starting parentEx.playerDataList loop to count if we have all players' data");
 		 for (PlayerData pd : parentEx.playerDataList )
 		 {
-			 trace ( "checking room & round");
+			 // trace ( "checking room & round");
 			 // continue if player data is not for current room or current round
 			 if (pd.room != roomId || pd.round != currentRound)	
 				 continue;
 			 
-			 trace ( "checking if room player is in pd list");
+			 // trace ( "checking if room player is in pd list");
 			 // add player data to list if user of player data is in current room
 			 if (playerRoom.getUserList().contains(pd.player) )
 			 {
 				 playerDataRound.add(pd);
-				 trace("player of this data row is in room, adding player data to temp list"); 
+				 // trace("player of this data row is in room, adding player data to temp list"); 
 			 }
 		 }
 		 
 		 // if list count equals users in room, then we got all data, send back
-		 trace("Checking if we have data for all " + playerCount + " players in the room, list size=" + playerDataRound.size());
+		 //trace("Checking if we have data for all " + playerCount + " players in the room, list size=" + playerDataRound.size());
 		 if (playerDataRound.size() == playerCount) 
 		 {
 			 trace("Got data from all " + playerCount +  " players in the room, sending result back for round " + currentRound);
@@ -91,16 +93,20 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 			 collection.putSFSObject("roundData", roundData);
 			 
 			 parentEx.send("receiveRoundData", collection , playerRoom.getUserList()   );
+			 for (PlayerData pd : playerDataRound )
+				 parentEx.playerDataList.remove(pd);
+			// trace ("Size of playerDataList at end: " + parentEx.playerDataList.size() );
 		 }
 		 else
-			 trace("Not all data was received");
+			 // trace("Not all data was received");
+		 	 trace("Got data for " + playerDataRound.size() +  " of the " + playerCount + " players in the room for round " + currentRound);
 		
 	}
 	
 	
 	public void AddPlayerDataToList(User player, SFSObject data, IdmExt parentEx)
 	{
-		trace("Adding playerdata to list");
+		// trace("Adding playerdata to list");
 		
 		int playerId = player.getPlayerId();
 		Room playerRoom = player.getLastJoinedRoom(); 
@@ -108,7 +114,6 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 		int currentRound = data.getInt("round");
 		String role = data.getUtfString("role");
 		
-		trace ("check");
 		PlayerData newPd = new PlayerData();
 		newPd.room = roomId;
 		newPd.playerId = playerId;
@@ -116,7 +121,6 @@ public class submitRoundDataHandler extends BaseClientRequestHandler {
 		newPd.player = player;
 		newPd.role = role;
 		newPd.playerRoundData = data;
-		trace ("check");
 		parentEx.playerDataList.add(newPd);
 	}
 
